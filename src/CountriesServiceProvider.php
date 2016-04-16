5a42ef18df970df7a0ef882386cd8d12658923a9
ernysans <ernysans@icloud.com>
2016-04-16 15:26:21 -0500

    Service Provider -- v1

---
+++ src/CountriesServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ErnySans\Countries;
+
+use Illuminate\Support\ServiceProvider;
+
+class CountriesServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/Migrations/' => database_path('migrations')
+        ], 'migrations');
+
+        $this->publishes([
+            __DIR__ . '/Seeds/' => database_path() . 'seeds'
+        ], 'seeds');
+    }
+}
