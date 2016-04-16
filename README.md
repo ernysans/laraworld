61adfc300d02f5edb7a51273974b178ce91a5340
ernysans <ernysans@icloud.com>
2016-04-16 17:51:31 -0500

    Model CleanUp and Basic README

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 Countries Package for Laravel 5.*
 
 ## Installation
-
 This package can be installed through Composer.
 
 ```bash
@@ -12,7 +11,6 @@ composer require ernysans/countries
 When using Laravel there is a service provider that you can make use of.
 
 ```php
-
 // app/config/app.php
 
 'providers' => [
@@ -27,14 +25,28 @@ Publish Migrations and Seeds
 php artisan vendor:publish --provider="ErnySans\Countries\CountriesServiceProvider"
 ```
 
+Prepare Seed.
+
+```php
+// database/seeds/DatabaseSeeder.php
+
+public function run()
+{
+    // Seed the countries
+    $this->call(CountriesTableSeeder::class);
+}
+```
+
 ## Use
+These methods is provided:
 
+* `getCountries()`: Get all the countries from the JSON file, you don't need to run migrations to get all the data in JSON format.
 
+When using Laravel you can work with the [Collection Available Methods](https://laravel.com/docs/master/collections).
 
 ## Credits
 
 - [Erny Sans](http://erny.co)
 
 ## License
-
 This package is open-sourced licensed under the [MIT license](http://opensource.org/licenses/MIT).
--- src/Models/Countries.php
+++ src/Models/Countries.php
@@ -41,7 +41,7 @@ class Countries extends Model
     ];
 
     /**
-     * Get the countries from the JSON file.
+     * Get all the countries from the JSON file.
      *
      * @return array
      */
@@ -56,67 +56,5 @@ class Countries extends Model
         //Return the Time Zones
         return $timeZones;
 
-    }
-
-    /**
-     * Returns one country from the JSON file.
-     *
-     * @param string $id The country id
-     *
-     * @return array
-     */
-    public function getOne($id)
-    {
-        $countries = $this->getCountries();
-        return $countries[$id];
-    }
-
-    /**
-     * Returns a list of countries from the JSON file.
-     *
-     * @param null $sort
-     * @return array
-     */
-    public function getList($sort = null)
-    {
-        //Get the countries list
-        $countries = $this->getCountries();
-
-        //Sorting
-        $validSorts = array(
-            'capital',
-            'citizenship',
-            'country-code',
-            'currency',
-            'currency_code',
-            'currency_sub_unit',
-            'full_name',
-            'iso_3166_2',
-            'iso_3166_3',
-            'name',
-            'region-code',
-            'sub-region-code',
-            'eea',
-            'calling_code',
-            'currency_symbol',
-            'flag'
-        );
-
-        if (!is_null($sort) && in_array($sort, $validSorts)) {
-            uasort($countries, function ($a, $b) use ($sort) {
-                if (!isset($a[$sort]) && !isset($b[$sort])) {
-                    return 0;
-                } elseif (!isset($a[$sort])) {
-                    return -1;
-                } elseif (!isset($b[$sort])) {
-                    return 1;
-                } else {
-                    return strcasecmp($a[$sort], $b[$sort]);
-                }
-            });
-        }
-
-        //Return the countries
-        return $countries;
     }
 }
