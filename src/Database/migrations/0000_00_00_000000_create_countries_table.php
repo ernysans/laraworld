2bda957bdad2e3dcda1dfb4b82f6b4b6ee2399e5
ernysans <ernysans@icloud.com>
2016-04-16 18:04:19 -0500

    JSON and Migration Update

--- src/Database/migrations/0000_00_00_000000_create_countries_table.php
+++ src/Database/migrations/0000_00_00_000000_create_countries_table.php
@@ -16,21 +16,21 @@ class CreateCountriesTable extends Migration
         Schema::create('countries', function(Blueprint $table)
         {
             $table->increments('id');
+            $table->string('name', 255)->default('');
+            $table->string('full_name', 255)->nullable();
             $table->string('capital', 255)->nullable();
             $table->string('citizenship', 255)->nullable();
-            $table->string('country_code', 3)->default('');
             $table->string('currency', 255)->nullable();
             $table->string('currency_code', 255)->nullable();
             $table->string('currency_sub_unit', 255)->nullable();
             $table->string('currency_symbol', 3)->nullable();
-            $table->string('full_name', 255)->nullable();
-            $table->string('iso_3166_2', 2)->default('');
-            $table->string('iso_3166_3', 3)->default('');
-            $table->string('name', 255)->default('');
+            $table->string('country_code', 3)->default('');
             $table->string('region_code', 3)->default('');
             $table->string('sub_region_code', 3)->default('');
             $table->boolean('eea')->default(0);
             $table->string('calling_code', 3)->nullable();
+            $table->string('iso_3166_2', 2)->default('');
+            $table->string('iso_3166_3', 3)->default('');
             $table->timestamps();
         });
     }
